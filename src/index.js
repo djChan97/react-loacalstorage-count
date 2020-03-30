93730257b04b5fc6c0a3d9c5402eaf2e1c0cf1d6
djChan97 <moldysaltedfish@gmail.com>
2020-03-30 22:37:30 +0800

    project: Update iconfont

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "../src/assets/css/normalize.css";
+import "../src/assets/js/iconfont";
 import "./index.css";
 import App from "./App";
 
