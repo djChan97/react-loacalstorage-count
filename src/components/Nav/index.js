1d24f1f28c3015ce745c0cd22a004e65e6d05bba
djChan97 <moldysaltedfish@gmail.com>
2020-03-30 22:37:52 +0800

    component: optimize Nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./index.scss";
 import Icon from "../Icon/index";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
 	const navClsPrefix = "nav";
 	return <div className={navClsPrefix}>
-		<span className={`${navClsPrefix}-item`}><Icon/></span>
-		<span className={`${navClsPrefix}-item`}>标签</span>
-		<span className={`${navClsPrefix}-item`}>统计</span>
+		<NavLink to={"/money"} activeClassName={"selected"}>
+			<Icon name={"money"}/>
+			记账
+		</NavLink>
+		<NavLink to={"/labels"} activeClassName={"selected"}>
+			<Icon name={"tag"}/>
+			标签
+		</NavLink>
+		<NavLink to={"/statistics"} activeClassName={"selected"}>
+			<Icon name={"chart"}/>
+			统计
+		</NavLink>
 	</div>;
 };
 
