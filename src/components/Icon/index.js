f1f5d9481b537c60500260d2359b5190ae0cbb7a
djChan97 <moldysaltedfish@gmail.com>
2020-03-30 16:23:45 +0800

    component: add Icon

---
+++ src/components/Icon/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+import Chart from "../icons/chart.svg";
+
+const Icon = () => {
+	const iconCLsPrefix = "icon";
+	return <div>
+		<Chart/>
+	</div>;
+};
+
+export default Icon;
